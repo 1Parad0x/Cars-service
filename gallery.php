1b185ec4e73972931b1f5c2d12f36ca7b997597f
Vladyslav Chornoivan <scrollgamerp@gmail.com>
2025-03-29 19:45:44 +0100

    footer & header

---
+++ gallery.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Motor Gallery</title>
+  <meta name="description" content="">
+  <?php
+  include("partials/header.php");
+  ?>
+      </section>
+      <section class="container margin-bottom-50">
+       <div class="row">
+        <h2 class="col-lg-12 text-center text-uppercase margin-bottom-30">Donec pede justo fringilla ulputate eget</h2>
+        <p class="col-lg-12 text-center margin-bottom-30">Aenean commodo ligula eget dolor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+        <img src="img/gallery-header.jpg" alt="Image description" class="img-responsive col-lg-12">        
+      </div>
+    </section>    
+
+    <!--Main content-->
+    <section class="container margin-bottom-50">
+      <div class="row">
+        <div class="tm-gallery col-lg-12">
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">A Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">B Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">C Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">E Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">S Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">G Class</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+          <div class="tm-item-container">
+            <img src="img/gallery/0.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>                    
+          <div class="tm-item-container">
+            <img src="img/gallery/1.jpg" alt="Image">
+            <div class="tm-item-price-container tm-gallery-item-info">
+              <span class="tm-item-price">Benz</span>
+              <a href="#" class="tm-item-link">
+                <span class="tm-item-action">View More</span>
+                <img src="img/plus.png" class="tm-item-add-icon" alt="Image">
+              </a>
+            </div>
+          </div>
+        </div>        
+      </div>
+
+      <!--banner-->
+      <div class="tm-banner">
+        <h2 class="tm-banner-title">Maecenas</h2>
+        <p class="tm-banner-description">Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium</p>
+        <a href="#" class="tm-banner-link"><i class="fa fa-play"></i></a>
+      </div>
+    </section> <!-- Main content -->
+    <!--Footer content-->
+      <?php
+      include("partials/footer.php");
+      ?>
+    <!-- Footer content-->
+    
+    <!-- JS -->
+    <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      <!-- jQuery -->
+    <script type="text/javascript" src="js/templatemo-script.js"></script>      <!-- Templatemo Script -->
+
+  </body>
+  </html>
---
+++ partials/header.php
@@ -0,0 +1,38 @@
+<!-- 
+Motor Template
+http://www.templatemo.com/tm-463-motor
+-->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<link href="css/templatemo-style.css" rel="stylesheet">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+</head>
+
+<body>
+  <section class="templatemo-top-section">
+    <div class="templatemo-header">
+      <img class="templatemo-header-img" src="img/header.png" alt="Header">
+      <h1 class="templatemo-site-name">Motor</h1>
+      <div class="mobile-menu-icon">
+        <i class="fa fa-bars"></i>
+      </div>
+      <div class="templatemo-nav-container">
+        <nav class="templatemo-nav">
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="products.php">Products</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li><a href="contact.php">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
