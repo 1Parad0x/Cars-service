f23837c98cd7c62bb85eaa8372d4c058f4790859
Vladyslav Chornoivan <scrollgamerp@gmail.com>
2025-05-06 13:54:46 +0200

    fix 1.0

---
+++ user_edit.php
@@ -0,0 +1,52 @@
+<?php
+include_once('partials/header.php');
+
+$db = new Database();
+$user = new User($db);
+
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $userData = $user->show($id);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $role = $_POST['role'];
+        if ($user->update_user($id, $name, $email, $role)) {
+            header("Location: admin.php");
+            exit;
+        } else {
+            echo "Error editing user.";
+        }
+    }
+} else {
+    echo "User ID not provided.";
+}
+?>
+
+<section class="container">
+    <h1>Edit User</h1>
+    <form method="POST" action="">
+        <label for="id">ID:</label>
+        <input type="text" id="id" name="id" value="<?php echo htmlspecialchars($userData['id']); ?>" readonly>
+        <br>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" value="<?php echo htmlspecialchars($userData['name']); ?>" required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($userData['email']); ?>" required>
+        <br>
+        <label for="role">Role:</label>
+        <select id="role" name="role">
+            <option value="0" <?php if ($userData['role'] == 0); ?>>Admin</option>
+            <option value="1" <?php if ($userData['role'] == 1); ?>>User</option>
+        </select>
+        <br>
+        <button type="submit">Edit</button>
+    </form>
+    <a href="admin.php">Back to Admin</a>
+
+</section>
+
+<?php
+include_once('partials/footer.php');
+?>
